dfbe391bba6d04b128c7d63c04ac594aabdadcf1
Mark Damm <73367557+markkdamm@users.noreply.github.com>
2022-08-11 17:35:15 +0200

    Comment the code

--- RoverImage.js
+++ RoverImage.js
@@ -3,38 +3,43 @@ const axios = require('axios')
 
 var obj;
 var objone;
-
+//randomize the query parameters
 let year = Math.floor(Math.random() * 8) + 2014
 let month= Math.floor(Math.random() * 11)+ 1
 let day = Math.floor(Math.random() * 25)+ 1
 let r = Math.floor(Math.random() * 3)
 
+//api data
 const start = `${year}-0${month}-${day}`
 const apikey = process.env.API_KEY;
 const apiurl = 'https://api.nasa.gov/';
-const path = [`mars-photos/api/v1/rovers/curiosity/photos?camera=fhaz&earth_date=${start}&api_key=${apikey}`]
+const path = `mars-photos/api/v1/rovers/curiosity/photos?camera=fhaz&earth_date=${start}&api_key=${apikey}`
 
+//store the data in global scope variable
 console.log(apiurl+ path)
 axios.get(apiurl + path).then((res)=> res.data.photos[r].img_src).then(data => obj = data).then(() => console.log(obj)).catch((error) => console.log(error))
 
-
+// delay do the api can return the values on time [else undfined]
 setTimeout(() => {
 
-require('dotenv').config()
+//get the local ip address [Not supported when using proxy services]
 const ip = require('ip')
 const ipAddress = ip.address()
 console.log(ipAddress)
+  
+//compose the request
 const http = require('http');
-const html = `<image src="${obj}"/>`
 const hostname = ipAddress;
 const port = 8080;
+const html = `<image src="${obj}"/>`
 
+//initialize the server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end(html);
 });
-
+//confirm the server it's being deployed and specify the port and hostname
 server.listen(port, hostname, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
